dacf88670ecf44a60107ba59a5c2971eaf6ccedf
Professeur <prof@j-c224m-ol-01.local>
2016-02-12 09:43:21 -0500

    Creation du projet

---
+++ ProjetRapidoRestoIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetRapidoRestoIOS
+//
+//  Created by Professeur on 2016-02-12.
+//  Copyright (c) 2016 RestoRapido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
