6e60bef57a369d59d908e902d5fee9f9bb348b55
francisverreault <verreault.frank@gmail.com>
2016-03-01 20:35:11 -0500

    Creation de l'apparence general tu tp

---
+++ ProjetRestoRapidoIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ProjetRestoRapidoIOS
+//
+//  Created by Device on 2016-03-01.
+//  Copyright © 2016 Device. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ProjetRestoRapidoIOSTests/ProjetRestoRapidoIOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjetRestoRapidoIOSTests.swift
+//  ProjetRestoRapidoIOSTests
+//
+//  Created by Device on 2016-03-01.
+//  Copyright © 2016 Device. All rights reserved.
+//
+
+import XCTest
+@testable import ProjetRestoRapidoIOS
+
+class ProjetRestoRapidoIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
