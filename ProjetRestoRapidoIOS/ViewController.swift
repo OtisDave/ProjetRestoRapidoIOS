05cd10f6fc38b2ff60f98ef03af63ed9f9910dab
Dave <dave@Daves-MacBook-Pro.local>
2016-03-04 20:54:06 -0500

    Vérification du login à l'aide de l'API fait par asp.net

--- ProjetRestoRapidoIOS/ViewController.swift
+++ ProjetRestoRapidoIOS/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-     
+    
+    
+    @IBOutlet weak var txtCourriel: UITextField!
+    @IBOutlet weak var txtMdp: UITextField!
+    
+    var Data: NSData? = nil
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    @IBAction func btnConnect(sender: AnyObject) {
+        if(txtCourriel.text != "" && txtMdp.text != ""){
+            Data = getJSON("https://www.googleapis.com/calendar/v3/calendars/calendarId")
+            if(Data != nil){
+                self.performSegueWithIdentifier("Menu", sender: nil)
+            }
+        }
+    }
+    
+    
+    //Pour obtenir le json
+    func getJSON(urlToRequest: String) -> NSData{
+        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
+    }}
 
