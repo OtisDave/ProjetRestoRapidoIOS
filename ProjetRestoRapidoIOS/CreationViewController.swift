6d70f3a97a5008c6e8effa71a8864980e52145c2
System Administrator <root@j-c224m-ol-01.local>
2016-03-11 12:28:11 -0500

    Création classe creationController

--- ProjetRestoRapidoIOS/CreationViewController.swift
+++ ProjetRestoRapidoIOS/CreationViewController.swift
@@ -11,8 +11,31 @@ import UIKit
 
 
 class CreationViewController : UIViewController {
+    
+    
+    @IBOutlet weak var txtPrenom: UITextField!
+    @IBOutlet weak var txtNom: UITextField!
+    @IBOutlet weak var txtCourriel: UITextField!
+    @IBOutlet weak var txtMdp: UITextField!
+    
+    
+    @IBOutlet weak var swiBle: UISwitch!
+    @IBOutlet weak var swiCoque: UISwitch!
+    @IBOutlet weak var swiLait: UISwitch!
+    @IBOutlet weak var swiPoisson: UISwitch!
+    @IBOutlet weak var swiOeuf: UISwitch!
+    @IBOutlet weak var swiSesame: UISwitch!
+    @IBOutlet weak var swiArachide: UISwitch!
+    @IBOutlet weak var swiCrustace: UISwitch!
+    @IBOutlet weak var swiSoja: UISwitch!
+    @IBOutlet weak var swiMollusque: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
    
+    }
+    
+    @IBAction func btnCreerClick(sender: AnyObject) {
     }
 }
